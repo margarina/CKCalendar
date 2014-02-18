71d6a8893f001f7197addc14d985262fb7b97d15
margarina <blaz@marg.si>
2014-02-18 18:22:17 +0100

    Remove header-highlight.

--- Source/CKCalendarView.m
+++ Source/CKCalendarView.m
@@ -147,12 +147,6 @@
     self.onlyShowCurrentMonth = YES;
     self.adaptHeightToNumberOfWeeksInMonth = YES;
 
-    UIView *highlight = [[UIView alloc] initWithFrame:CGRectZero];
-    highlight.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.2];
-    highlight.layer.cornerRadius = 6.0f;
-    [self addSubview:highlight];
-    self.highlight = highlight;
-
     // SET UP THE HEADER
     UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
     titleLabel.textAlignment = NSTextAlignmentCenter;
